5411212b3f1aca371acb4417ec8778a8ce4e9129
Katherine De La Cruz <kdelacruz@rcp.pe>
2017-05-09 14:19:58 -0500

    Add icon to marker

--- index.android.js
+++ index.android.js
@@ -9,23 +9,22 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions
 } from 'react-native';
+const {height, width} = Dimensions.get('window');
+import MapView from 'react-native-maps';
 
 export default class reactNative_map extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <MapView style={styles.map} initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}/>
       </View>
     );
   }
@@ -38,15 +37,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  map: {
+    width: width,
+    height: height,
   },
 });
 
--- index.ios.js
+++ index.ios.js
@@ -9,23 +9,85 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions
 } from 'react-native';
 
+const {height, width} = Dimensions.get('window');
+
+import MapView from 'react-native-maps';
+
 export default class reactNative_map extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: -12.1164923,
+        longitude: -77.02417560000004,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      },
+      markers: [
+        {
+          latlng: {
+            latitude: -12.117376010209252,
+            longitude: -77.02246427536011,
+          },
+          title: 'Carritos',
+          description: 'Estacionamiento 1'
+        },
+        {
+          latlng: {
+            latitude: -12.116117232017405,
+            longitude: -77.02396631240845,
+          },
+          title: 'Parking',
+          description: 'Estacionamiento 2'
+        },
+        {
+          latlng: {
+            latitude: -12.117858540275225,
+            longitude: -77.02471733093262,
+          },
+          title: 'Surquillo parking',
+          description: 'Estacionamiento 3'
+        }
+      ]
+    };
+    this.onRegionChange = this.onRegionChange.bind(this);
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <MapView style={styles.map}
+          showsUserLocation={true}
+          followsUserLocation={true}
+          showsMyLocationButton={true}
+          showsCompass={false}
+          showsPointsOfInterest={false}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+        >
+          {this.state.markers.map((marker, i) => (
+            <MapView.Marker
+              key={i}
+              coordinate={marker.latlng}
+              image={require('./images/icon.png')}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
+        </MapView>
+        <View style={styles.container}>
+          <Text>Latitud: {this.state.region.latitude}</Text>
+          <Text>Longitud: {this.state.region.longitude}</Text>
+        </View>
       </View>
     );
   }
@@ -38,15 +100,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  map: {
+    width: width,
+    height: height - 90,
   },
 });
 
