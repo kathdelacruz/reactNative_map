16a723221b136dac31b66f795a17e35d24a4f764
Katherine De La Cruz <kdelacruz@rcp.pe>
2017-05-09 17:32:20 -0500

    Add polyline

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ import {
 const {height, width} = Dimensions.get('window');
 
 import MapView from 'react-native-maps';
+import Polyline from '@mapbox/polyline';
 
 export default class reactNative_map extends Component {
 
@@ -62,11 +63,50 @@ export default class reactNative_map extends Component {
     this.onRegionChange = this.onRegionChange.bind(this);
   }
 
+
   onRegionChange(region) {
     this.setState({ region });
   }
 
+  getDirections() {
+    var url = 'https://maps.googleapis.com/maps/api/directions/json?mode=driving&origin=-12.117859,-77.024717&destination=-12.11832,-77.022271&key=AIzaSyCQLivZ-FMeEMkJqlZniZldMJwmfiw-8XY';
+
+    return new Promise((resolve, reject) => {;
+      fetch(url)
+      .then((response) => {
+        return response.json();
+      }).then((json) => {
+        resolve(json);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.getDirections().then((data) => {
+      if (data.status !== 'OK') {
+            return [];
+          }
+
+          let points = data.routes[0].overview_polyline.points;
+          let steps = Polyline.decode(points);
+          let polylineCoords = [];
+
+          for (let i=0; i < steps.length; i++) {
+            let tempLocation = {
+              latitude : steps[i][0],
+              longitude : steps[i][1]
+            }
+            polylineCoords.push(tempLocation);
+          }
+
+          this.setState({polylineCoords});
+    });
+  }
+
   render() {
+    let coordinates = this.state.markers.map(marker => marker.latlng);
     return (
       <View style={styles.container}>
         <MapView style={styles.map}
@@ -93,6 +133,11 @@ export default class reactNative_map extends Component {
               </MapView.Callout>
             </MapView.Marker>
           ))}
+          <MapView.Polyline
+            coordinates={this.state.polylineCoords}
+            strokeWidth={3}
+            strokeColor="blue"
+           />
         </MapView>
         <View style={styles.container}>
           <Text>Latitud: {this.state.region.latitude}</Text>
